0c9a2edbd43460dbba864fa8f3a7c7962ca26db5
Starninjal <BR17johnn@gmail.com>
2023-06-20 18:55:16 -0300

    end edit

--- efetivado.js
+++ efetivado.js
@@ -1,22 +1,20 @@
+var intervalo = setTimeout(() => {
+    document.getElementById("text1").innerText = "PARABÉNS VOCÊ COMPLETOU O DESAFIO";
+}, 4000);
+
+setTimeout(() => {
+    document.getElementById("text2").innerText = "OBRIGADO POR JOGAR E ATÉ A PRÓXIMA FASE";
+}, 4200);
+
+setTimeout(() => {
+    document.getElementById("text3").innerText = "HAHAHAHAHAHAHHAHAHAHAHAHAHAHHAHAHAHAHAHHA";
+}, 4400);
+
+setTimeout(() => {
+    document.getElementById("text4").innerText = "LEIA(RISADA MALIGNA)"
+}, 4600);
 
 
-function extractPageNumberFromUrl(url) {
-    const regex = /page(\d+)\.html/;
-    const matches = url.match(regex);
-    if (matches && matches.length > 1) {
-        return parseInt(matches[1]);
-    }
-    return null;
-}
-
-function goToNextQuestion() {
-    const currentPageNumber = extractPageNumberFromUrl(document.referrer);
-    if (currentPageNumber !== null) {
-        const nextPageNumber = currentPageNumber + 1;
-        const nextPageUrl = `page${nextPageNumber}.html`;
-        alert(nextPageUrl)
-        window.location.href = nextPageUrl;
-    }
-}
-
-
+//setInterval(binToText("text2", "CHEIO DE INSETOS! EU TE CONTRATEI PARA"), 1000);
+//setInterval(binToText("text3", "ELIMINÁ-LOS. AGORA ME DIGA QUE FERRAMENTA"), 1000);
+//setInterval(binToText("text4", "VOCÊ VAI UTILIZAR!!!"), 1000);
